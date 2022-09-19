3053786446b0f190eba21e1207c44a35ecd8a280
glory <aremuopomulero@gmail.com>
2022-09-19 01:03:09 +0100

    changes

--- src/utils/mongourl.ts
+++ src/utils/mongourl.ts
@@ -2,7 +2,7 @@ const mongourl: { url: string } = { url: 'mongodb://localhost/zoomba' };
 const env = process.env.APP_ENV;
 if (env == 'development') {
   mongourl.url =
-    'mongodb+srv://glorhie33:Oluwadunmininu@33@cluster0.ygqefen.mongodb.net/?retryWrites=true&w=majority';
+    'mongodb+srv://glorhie33:oluwadunmininu@cluster0.ygqefen.mongodb.net/?retryWrites=true&w=majority';
 } else if (env == 'production') {
   mongourl.url = '';
 }
