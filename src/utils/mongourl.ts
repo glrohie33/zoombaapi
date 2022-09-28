88124a13e4afbae8c4fb1e735def170f801e922b
glory <aremuopomulero@gmail.com>
2022-09-28 19:08:04 +0100

    changes

--- src/utils/mongourl.ts
+++ src/utils/mongourl.ts
@@ -2,7 +2,7 @@ const mongourl: { url: string } = { url: 'mongodb://localhost/zoomba' };
 const env = process.env.APP_ENV;
 if (env == 'development') {
   mongourl.url =
-    'mongodb://localhost/test-zoomba';
+    'mongodb://zoomba1:Zoomba123@localhost/test-zoomba?authSource=admin';
 } else if (env == 'production') {
   mongourl.url = '';
 }
